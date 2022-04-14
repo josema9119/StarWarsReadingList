eec5c462f3405147a7ca416e40071ff954bd07ae
josema9119 <jose.ma.mm.91@gmail.com>
2022-04-14 10:52:46 +0000

    navbar

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,17 +1,29 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
-			</div>
-		</nav>
-	);
+  return (
+    <nav className="navbar navbar-light bg-light mb-2 justify-content-around">
+      <h1>STAR WARS LOGO</h1>
+      <div className="ml-auto">
+          <div className="dropdown">
+            <button
+              className="btn btn-primary dropdown-toggle"
+              type="button"
+              id="dropDownMenu"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              Favorites
+            </button>
+            <ul className="dropdown-menu" aria-labelledby="dropDownMenu">
+              <li>
+                <a className="dropdown-item" href="#">
+                  (empty)
+                </a>
+              </li>
+            </ul>
+          </div>
+      </div>
+    </nav>
+  );
 };
