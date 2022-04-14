03a6052eade6dbeddb2e44d8fc457f8d01474e2e
josema9119 <jose.ma.mm.91@gmail.com>
2022-04-14 14:17:40 +0000

    second change

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,37 @@
-import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+import React,{useContext, useEffect} from "react";
+import {Link} from "react-router-dom";
 import "../../styles/home.css";
+import { Context } from "../store/appContext";
+import reactRouterDom from "react-router-dom";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+
+export const Home = () => {
+	const { store, actions } = useContext(Context);
+
+	useEffect (()=>{
+		actions.getPeople()
+
+	}, [])
+   return (
+	   <div>
+	   <h1>People</h1>
+	   <div className="row">
+	   {store.people.map((people, i)=>{
+
+		   return <div key={i} className="card" style={{width: "18rem"}}>
+						<img src="..." className="card-img-top" alt="..."/>
+						<div className="card-body">
+							<h5 className="card-title">{people.name}</h5>
+							<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+							<Link to="/single/" + people.uid>
+								<button className="btn btn-primary">More info</button>
+							</Link>
+						</div>
+		 </div>
+		})}
+		</div>
+		</div>
+   )
+    
+};
+
