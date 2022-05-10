9badfb324756f1759283afefbbd8130a65d2de68
josema9119 <jose.ma.mm.91@gmail.com>
2022-05-10 18:59:29 +0000

    class areglado

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -4,7 +4,7 @@ import "../../styles/footer.css";
 export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center">
 		<p className="paragraph">
-		<i class="fab fa-jedi-order"></i> MAY THE FORCE BE WITH YOU <i class="fab fa-jedi-order"></i>
+		<i className="fab fa-jedi-order"></i> MAY THE FORCE BE WITH YOU <i class="fab fa-jedi-order"></i>
 		</p>
 	</footer>
 );
